1f637e9e00b5ca5d18fd33d01ae3e31ab2e924f4
ayaheka <47730029+ayaheka@users.noreply.github.com>
2022-06-10 14:47:04 +0700

    add backup vendor module

--- main.go
+++ main.go
@@ -1,1 +1,43 @@
+package main
 
+import (
+	"encoding/xml"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Person struct {
+	XMLName   xml.Name `xml:"person"`
+	FirstName string   `xml:"firstName,attr"`
+	LastName  string   `xml:"lastName,attr"`
+}
+
+func IndexHandler(c *gin.Context) {
+	// c.JSON(200, gin.H{
+	// "message": "hello world",
+	// })
+
+	// name := c.Params.ByName("name")
+	// c.JSON(200, gin.H{
+	// 	"message": "hello " + name,
+	// })
+
+	c.XML(200, Person{FirstName: "Abdul", LastName: "Somad"})
+
+}
+
+func main() {
+	// router := gin.Default()
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": "helloq-workd",
+	// 	})
+	// })
+
+	router := gin.Default()
+	// router.GET("/", IndexHandler)
+	// router.GET("/:name", IndexHandler)
+	router.GET("/", IndexHandler)
+
+	router.Run(":5000")
+}
